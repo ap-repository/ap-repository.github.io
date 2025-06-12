af861222e5005b42ad9f7863dfc0ff31da7ddabd
Jari van Wijk <syftelurth@gmail.com>
2025-06-12 02:14:45 +0200

    Updates

---
+++ assets/about-B5ginfcx.js
@@ -0,0 +1,1 @@
+import{j as o}from"./index-B_8AXO0z.js";const e=function(){return o.jsx("div",{className:"p-2",children:"Hello from About!"})};export{e as component};
--- assets/index-BneML9dY.js
+++
@@ -1,1 +0,0 @@
-import{j as n}from"./index-B49I_FTc.js";const o=()=>n.jsx("div",{children:"Dashboard"}),s=function(){return n.jsx(o,{})};export{s as component};
